7298b2babad34763e780b5b7955ccbf2b9ecf07f
kote1999 <kotekirkitadze@gmail.com>
2021-11-21 00:49:06 +0400

    most logic for auth service is implemented

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private user: any;
+  private authState: any;
+
+  get currentUserId(): string {
+    return this.authState != null ? this.authState.uid : ''
+  }
+
+  constructor(private fireAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router) {
+    this.user = fireAuth.user;
+  }
+
+  signUp(email: string, password: string, displayName: string) {
+    return of(this.fireAuth.createUserWithEmailAndPassword(email, password))
+      .pipe(tap(user => {
+        this.authState = user;
+        const status = 'online';
+        this.setUserData(email, displayName, status)
+      }))
+  }
+
+  setUserData(email: string, displayName: string, status: string) {
+    const path = `users/${this.currentUserId}`;
+    const data = {
+      email,
+      displayName,
+      status
+    }
+    of(this.db.doc(path).update(data))
+  }
+
+  login(email: string, password: string) {
+    return of(this.fireAuth.signInWithEmailAndPassword(email, password))
+      .pipe(tap(d => {
+        const status = 'online';
+        this.setUserStatus(email, status);
+        this.router.navigate(['chat'])
+      }))
+  }
+
+  setUserStatus(email: string, status: string) {
+    const path = `users/${this.currentUserId}`;
+    const data = {
+      status: status
+    };
+  }
 }
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.scss']
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent {
 
-  constructor() { }
+  email: string;
+  password: string;
+  displayName: string;
+  errorMsg: string;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  signUp() {
+    const email = this.email;
+    const password = this.password;
+    const displayName = this.displayName;
+    this.authService.signUp(email, password, displayName).subscribe(
+      () => this.router.navigate(['chat']),
+      (err) => this.errorMsg = err
+    )
+
   }
 
 }
