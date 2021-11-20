59b69f0be1e49ad74c172db388ca7a33f4564013
kote1999 <kotekirkitadze@gmail.com>
2021-11-20 18:06:47 +0400

    ready for styling and auth service

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChatMessage } from '../models/chat-message.model';
 
 @Component({
   selector: 'app-message',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
+
+  @Input() chatMessage: ChatMessage;
 
   constructor() { }
 
